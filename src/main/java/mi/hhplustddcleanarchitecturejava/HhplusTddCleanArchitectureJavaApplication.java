2da78a44ccb30d68ccfabd0fd158355cfa99e4a3
klettermi <mi_aa_@naver.com>
2024-12-22 19:23:33 +0900

    init

---
+++ src/main/java/mi/hhplustddcleanarchitecturejava/HhplusTddCleanArchitectureJavaApplication.java
@@ -0,0 +1,13 @@
+package mi.hhplustddcleanarchitecturejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusTddCleanArchitectureJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusTddCleanArchitectureJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mi/hhplustddcleanarchitecturejava/HhplusTddCleanArchitectureJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package mi.hhplustddcleanarchitecturejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusTddCleanArchitectureJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
